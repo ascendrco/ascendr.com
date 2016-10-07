d6e784e44e9592a14c23218893f66c3c4c9ad6c4
Scott Lanning <skyout@gmail.com>
2016-10-07 15:04:49 -0400

    mkt site forms

--- js/global.js
+++ js/global.js
@@ -1,4 +1,26 @@
+// query string param getter
+(function($) {
+    $.QueryString = (function(a) {
+        if (a == "") return {};
+        var b = {};
+        for (var i = 0; i < a.length; ++i)
+        {
+            var p=a[i].split('=', 2);
+            if (p.length != 2) continue;
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+        }
+        return b;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
 (function($){
+
+    // if query string and email field, set the field
+    if ($.QueryString.email && $("#email").length) {
+        // set value
+        $("#email").val($.QueryString.email);
+    }
+
     // more button on homepage
     $('body').on('click', '#more', function (e) {
         e.preventDefault();
@@ -22,7 +44,8 @@
         e.preventDefault();
 
         // remove error clases
-        $('#signup input').removeClass('error');
+        $('#signup .error-message').hide();
+        $('#signup input, #signup label').removeClass('error');
 
         // if honey pot is full, we're done
         if ($('#honey').val() !== '') { return false; }
@@ -30,8 +53,9 @@
         // regex to test
         var emailRegex = new RegExp(/^[_a-z0-9_-]+(\.[_a-z0-9_-]+)*@[a-z0-9_-]+(\.[a-z0-9_-]+)*(\.[a-z]{2,4})+$/i);
         var charsRegex = new RegExp(/^([a-zA-Z0-9\/\\\_\-\.\,\(\)\:\;\$\!\s\']{1,100})$/);
+        var phoneRegex = new RegExp(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/);
 
-        // array to handle erros
+        // array to handle errors
         var errArr = [];
 
         // array to hold values
@@ -48,13 +72,18 @@
             if (key === 'email') {
                 if (!emailRegex.test(value) || value.length === 0) {
                     errArr.push(key);
-                    $('#' + key).addClass('error');
+                    $('#' + key + ', label[for=' + key + ']').addClass('error');
                 }
             // otherwise just test values exist
+            } else if (key === 'phone') {
+                if (!phoneRegex.test(value) || value.length === 0) {
+                    errArr.push(key);
+                    $('#' + key + ', label[for=' + key + ']').addClass('error');
+                }
             } else {
                 if (!charsRegex.test(value) || value.length === 0) {
                     errArr.push(key);
-                    $('#' + key).addClass('error');
+                    $('#' + key + ', label[for=' + key + ']').addClass('error');
                 }
             }
         });
@@ -65,12 +94,12 @@
                 jsonp: 'callback',
                 dataType: 'jsonp',
                 data: values,
-                success: function( response ) {
-                    $('#signup').hide()
-                    $('#success').show()
+                success: function(response) {
+                    $('#signup').remove();
+                    $('#success').show();
                 },
                 error: function (jqXHR, textStats, err) {
-                    console.log(jqXHR, textStats, err);
+                    $("#signup .error-message").show();
                 }
             });
         }
