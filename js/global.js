0813c0d80ccdd258f65a32a4ad3ae26bf9336c2d
Scott Lanning <skyout@gmail.com>
2016-10-07 11:26:03 -0400

    responsive styling

--- js/global.js
+++ js/global.js
@@ -1,10 +1,21 @@
 (function($){
     // more button on homepage
-    $("body").on('click', '#more', function (e) {
+    $('body').on('click', '#more', function (e) {
         e.preventDefault();
         // scroll down to content
         $('html, body').animate({
             scrollTop: $('.row.companies').offset().top
         }, 500);
+    });
+    // responsive menu
+    $('body').on('click', '#menu', function (e) {
+        e.preventDefault();
+        var selector = $("#global_nav, #global_header");
+        // hide or show nav
+        if (selector.hasClass('active')) {
+            selector.removeClass('active');
+        } else {
+            selector.addClass('active');
+        }
     });
 })(jQuery);
